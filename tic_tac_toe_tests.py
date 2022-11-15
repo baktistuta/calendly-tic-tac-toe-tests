edf206b5bc54eafdd96a81748276db0ffe79da45
Baktistuta <baktistuta@Jordans-MBP-2.gpb>
2022-11-15 14:28:45 -0500

    Adding test files

---
+++ tic_tac_toe_tests.py
@@ -0,0 +1,128 @@
+import math
+import random
+from itertools import cycle
+from seleniumbase import BaseCase
+
+
+test_url = "https://roomy-fire-houseboat.glitch.me/"
+default_board_size = 3
+possible_board_sizes = range(3, 11)  # Assuming that your board size can be from 3x3 to 10x10
+char_to_use = cycle(["X", "O"])      # X always goes first
+cell_element_by_xpath = "//*[@id='{}']"
+endgame_message = "Congratulations player {}! You've won. Refresh to play again!"
+
+class TicTacToeTestClass(BaseCase):
+    def test_player_can_generate_board(self):
+        board_size = random.choice(possible_board_sizes)
+        self.setup_empty_board(board_size)
+        self.assert_correct_board_size(board_size)
+
+    def test_invalid_board_size_inputs(self):
+        self.setup_empty_board(-3)      # Testing negative integer
+        self.assert_no_board_on_page()
+        self.setup_empty_board(-2.33)   # Testing negative float
+        self.assert_no_board_on_page()
+        self.setup_empty_board(0)       # Testing 0
+        self.assert_no_board_on_page()
+        self.setup_empty_board(None)    # Testing null input
+        self.assert_no_board_on_page()
+    
+    def test_correct_message_for_winner(self):
+        self.setup_empty_board(default_board_size)
+        self.let_winner_be("X")
+        self.assert_exact_text(winner_msg, "#endgame")  # Expect to Fail due to bug in app
+        self.refresh()
+        self.setup_empty_board(default_board_size)
+        self.let_winner_be("O")
+        self.assert_exact_text(winner_msg, "#endgame")
+
+    def test_refreshing_page_clears_page(self):
+        self.setup_empty_board(default_board_size)
+        self.let_winner_be("X")
+        self.refresh()
+        self.assert_text_not_visible(winner_msg)
+        self.assert_no_board_on_page()
+
+    def test_player_is_not_shown_msg_after_a_draw(self):
+        self.setup_empty_board(default_board_size)
+        self.end_game_in_a_draw()
+        self.assert_text_not_visible(
+            winner_msg.format("X") or winner_msg.format("O")
+        )
+    
+    """
+    Helper method to generate an empty board to play.
+
+    {board_size} should be a positive integer or float.
+                 If the input type is 'float', it will be rounded up to the nearest integer.
+                 If the input type is 'None', it will click the start button without any input.
+    """
+    def setup_empty_board(self, board_size):
+        self.open(test_url)
+        if type(board_size) in [int, float] and board_size > 0:
+            if type(board_size) == float:
+                board_size = math.ceil(board_size)  # Float input is rounded up
+            self.type("#number", board_size)
+            self.click("#start")
+            self.assert_element("#table")
+        elif board_size == None:
+            self.click("#start")
+        else:
+            print("{} {}is an invalid board size.".format(board_size, type(board_size)))
+
+    """
+    Helper method to simulate a player winning a game.
+
+    String {player} should be either string "X" or "O".
+    """
+    def let_winner_be(self, player):
+        plays_for_X_to_win = [0, 3, 1, 4, 2]    # Player X completes a row at the top
+        plays_for_O_to_win = [5, 2, 8, 4, 7, 6] # Player O completes a diagonal from top right to bottom left
+
+        if player == "X":
+            cells_by_id_to_fill_in_order = plays_for_X_to_win
+            for id in cells_by_id_to_fill_in_order:
+                self.click(cell_element_by_xpath.format(id))
+                self.assert_text(next(char_to_use), cell_element_by_xpath.format(id))
+            next(char_to_use)  # Call this one more time to reset iterator
+            # self.format_msg_for_winner(player)
+        elif player == "O":
+            cells_by_id_to_fill_in_order = plays_for_O_to_win
+            for id in cells_by_id_to_fill_in_order:
+                self.click(cell_element_by_xpath.format(id))
+                self.assert_text(next(char_to_use), cell_element_by_xpath.format(id))
+            # self.format_msg_for_winner(player)
+        else:
+            print("{} is an invalid player".format(player))
+    
+        global winner_msg
+        winner_msg = endgame_message
+        winner_msg = winner_msg.format(player)
+
+    """
+    Helper method to simulate a draw.
+    """
+    def end_game_in_a_draw(self):
+        cells_by_id_to_fill_in_order = [0, 1, 3, 4, 7, 6, 2, 5, 8]
+        for id in cells_by_id_to_fill_in_order:
+            self.click(cell_element_by_xpath.format(id))
+
+    """
+    Assertion to check that the number of rows and columns match the expected size.
+    """
+    def assert_correct_board_size(self, expected_size):
+        num_rows = len (self.driver.find_elements("xpath", "//*[@id='table']/tr"))
+        num_columns = len (self.driver.find_elements("xpath", "//*[@id='table']/tr[1]/td"))
+        if type(expected_size) == float:
+            expected_size = math.ceil(expected_size)
+        print("Generated board size: {} x {}".format(num_rows, num_columns))
+        print("Expected board size: ", expected_size)
+        self.assert_true(num_rows == num_columns == expected_size)
+    
+    """
+    Assertion to check that the web page is clean and no Tic Tac Toe board is visible.
+
+    Checks for cell with id 0, which is the top left cell of a visible board.
+    """
+    def assert_no_board_on_page(self):
+        self.assert_element_not_visible(cell_element_by_xpath, "0")
